4dd06390bbdfd1279f76b31f83c3b57ae4fe8a72
ColeFrancis <155692856+ColeFrancis@users.noreply.github.com>
2024-12-12 15:38:31 -0700

    Update register.sv

--- Hardware/HDL/register.sv
+++ Hardware/HDL/register.sv
@@ -17,3 +17,30 @@ module register #(
     assign out = en ? mem : 'z;
        
 endmodule
+
+module vector_reg #(
+        parameter BITS = 8,
+        parameter N = 64
+    )(
+        input logic [BITS-1:0] in [N-1:0],
+        input logic set,
+        input logic en,
+        input logic clk,
+        output logic [BITS-1:0] out [N-1:0]
+    );
+    
+    genvar i;
+    generate
+        for (i=0; i<N; i++) begin
+            register #(
+                .BITS(BITS)
+            ) r(
+                .in(in[i]),
+                .set(set),
+                .en(en),
+                .clk(clk),
+                .out(out[i])
+            );
+        end
+    endgenerate
+endmodule
